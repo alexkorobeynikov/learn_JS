6dd19cb200774c2066b80323851cf5ce476ccb67
alexkorobeynikov <alexkorobeynikov@gmail.com>
2021-12-22 19:10:26 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+"use strict";
+let number = 6;
+
+const person = "Alex";
+
+const bool = true;
+
+const obj = {
+    name: "sasha",
+    age: 32,
+    isMarried: false
+};
+
+console.log(obj.name, obj.age);
+
+ let arr = ['sasha', 'dima', 'lena', 6, ];
+
+// alert('hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt("Are you 18?", "");
+// console.log(answer + 5);
+
+const answers = [];
+// answers[0] = prompt("как ваше имя?", "");
+// answers[1] = prompt("Your age", "");
+// answers[2] = prompt("Your lastname", "");
+
+console.log(typeof(answers));
+
+
+const user = 'sasha'; 
+alert(`привет ${user}`);
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(--incr);
+console.log(++decr);
+
+const isCheckIt = true,
+        isClose = false;
+
+console.log(isCheckIt && isClose);
+
+
+
