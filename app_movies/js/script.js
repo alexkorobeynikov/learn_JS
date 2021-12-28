4c45258ad2882a75ef216be472d9cf1795833031
alexkorobeynikov <alexkorobeynikov@gmail.com>
2021-12-28 20:53:12 +0400

    task1

--- app_movies/js/script.js
+++ app_movies/js/script.js
@@ -10,11 +10,19 @@ const personalMovieDB = {
     privat: false
 
 };
+let lastMovie = [];
+let ratingLastMovie = [];
+for(let i = 1; i <= 2; i++) {
+    lastMovie[i] = prompt("Один из последних просмотренных фильмов?", "");
+    ratingLastMovie[i] = prompt("Насколько оцените его?", "");
+    personalMovieDB.movies[lastMovie[i]] = ratingLastMovie[i];
 
-const lastMovie1 = prompt("Один из последник просмотренных фильмов?", ""),
-      ratingLastMovie1 = prompt("Насколько оцените его?", ""),
-      lastMovie2 = prompt("Один из последник просмотренных фильмов?", ""),
-      ratingLastMovie2 = prompt("Насколько оцените его?", "");  
+}
+
+// const lastMovie1 = prompt("Один из последник просмотренных фильмов?", ""),
+//       ratingLastMovie1 = prompt("Насколько оцените его?", ""),
+//       lastMovie2 = prompt("Один из последник просмотренных фильмов?", ""),
+//       ratingLastMovie2 = prompt("Насколько оцените его?", "");  
 
 // personalMovieDB.movies = {
 //     lastMovie1: ratingLastMovie1,
@@ -22,8 +30,8 @@ const lastMovie1 = prompt("Один из последник просмотрен
 
 // };
 
-personalMovieDB.movies[lastMovie1] = ratingLastMovie1;
-personalMovieDB.movies[lastMovie2] = ratingLastMovie2;
+// personalMovieDB.movies[lastMovie1] = ratingLastMovie1;
+// personalMovieDB.movies[lastMovie2] = ratingLastMovie2;
 
 console.log(personalMovieDB);
 
