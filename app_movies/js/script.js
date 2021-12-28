1cadbc543dbfa2d965e7f2201c3d08a9c27ca0ea
alexkorobeynikov <alexkorobeynikov@gmail.com>
2021-12-28 21:18:36 +0400

    task2

--- app_movies/js/script.js
+++ app_movies/js/script.js
@@ -2,6 +2,7 @@
 
 const numberOfFilms = +prompt("Сколько фильмов Вы посмотрели?", "");
 
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -10,14 +11,32 @@ const personalMovieDB = {
     privat: false
 
 };
+
+if (personalMovieDB.count < 10 ) {
+    alert("Слишком мало фильмов");
+}
+if (30 >= personalMovieDB.count >= 10 ) {
+    alert("Просмотрено довольно мало фильмов");
+}
+if ( personalMovieDB.count >= 10 && personalMovieDB.count <= 30 ) {
+    alert("Вы классический зритель");
+}
+if (personalMovieDB.count > 30 ) {
+    alert("Вы киноман");
+}
+
+
+
 let lastMovie = [];
 let ratingLastMovie = [];
-for(let i = 1; i <= 2; i++) {
+for (let i = 1; i <= 2; i++) {
     lastMovie[i] = prompt("Один из последних просмотренных фильмов?", "");
     ratingLastMovie[i] = prompt("Насколько оцените его?", "");
     personalMovieDB.movies[lastMovie[i]] = ratingLastMovie[i];
 
 }
+
+
 
 // const lastMovie1 = prompt("Один из последник просмотренных фильмов?", ""),
 //       ratingLastMovie1 = prompt("Насколько оцените его?", ""),
